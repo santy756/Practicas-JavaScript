92e1acec96190d898b14785688d04cd0a0b7ceed
santy756 <santyperez56@gmail.com>
2023-02-19 08:16:55 -0300

    Practica Array - For

---
+++ Practica 3/03_for.js
@@ -0,0 +1,71 @@
+//EJERCICIO 1
+
+console.log("EJERCICIO 1")
+
+let valoresRandom = [20, 55, 33, 12, 17 ];
+let acumulador = 0;
+
+for (let i = 0; i < valoresRandom.length; i++) {
+    acumulador += valoresRandom[i];
+    if (acumulador %2===0 ) {
+        console.log (acumulador + " Es par");
+    } else {
+        console.log(acumulador + " Es impar")
+    }
+}
+
+//EJERCICIO 2
+
+console.log("EJERCICIO 2")
+
+let palabras = ["Sueño", "Hola", "Casa", "Tarde", "Frio", "Mundo","Celular"];  
+let holamundo = "";
+let hola = "";
+let mundo = "";
+
+for (let index2 = 0; index2<palabras.length; index2++) {
+    if (palabras[index2] == "Hola") {
+        holamundo += palabras[index2]
+    } else if (palabras[index2] == "Mundo") {
+        holamundo += palabras[index2]
+    }
+} console.log(holamundo)
+
+
+for (let i=0; i<palabras.length; i++) {
+    if (palabras[i]== "Hola" || palabras[i] == "Mundo") {
+        
+    }
+} 
+
+
+//EJERCICIO 3
+
+console.log("EJERCICIO 3")
+
+let peces = [15, 65, 78, 11, 9];
+let maspesado = 1;
+let masliviano = 999;
+
+for (let i = 0; i<peces.length; i++) {
+    if (maspesado<peces[i]) {
+        maspesado = peces[i]        
+    } else if (masliviano>peces[i]) {
+        masliviano = peces[i]
+    }
+}console.log("El pez mas pesado es de " + maspesado+ " Kg")
+console.log("El pez mas liviano es de " + masliviano+ " Kg")
+
+
+//EJERCICIO 4
+
+console.log("EJERCICIO 4")
+
+let base = 2;
+let resultado = 1;
+
+for (let i = 0; i <8; i++) {
+    resultado = resultado * base;
+}
+
+console.log(resultado);
